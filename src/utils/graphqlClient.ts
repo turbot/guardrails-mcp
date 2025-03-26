82e8c7e18a460ee629a51ec922bc68439de95f8c
Nathan Wallace <nathan@turbot.com>
2025-03-26 14:42:01 -0400

    cleanup: remove debug logging and simplify auth header construction

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -1,10 +1,11 @@
 import { GraphQLClient } from "graphql-request";
+import { Buffer } from "buffer";
 import config from "../config/env.js";
 
 // Create a singleton GraphQL client
 const graphqlClient = new GraphQLClient(config.TURBOT_GRAPHQL_ENDPOINT, {
   headers: {
-    authorization: "Basic " + btoa(encodeURIComponent(`${config.TURBOT_ACCESS_KEY_ID}:${config.TURBOT_SECRET_ACCESS_KEY}`)),
+    authorization: "Basic " + Buffer.from(`${config.TURBOT_ACCESS_KEY_ID}:${config.TURBOT_SECRET_ACCESS_KEY}`).toString("base64"),
   },
 });
 
